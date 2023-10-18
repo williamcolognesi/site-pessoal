8f2a1e458897a8341b49cd2ffc02e2401caad1bf
William Colognesi <williamcolognesi.developer@gmail.com>
2023-10-18 19:09:28 -0300

    feat: customizando header mobile

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,33 +1,62 @@
 import { Container, Grid } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import * as S from "./styles";
 import Logo from "../../../src/assets/img/Logo.png";
+import { useState } from "react";
+import useWindowSize from "../lib/useWindowSize";
 
 export default function Header() {
+  const [isMobileMenu, setMobileMenu] = useState(false);
+  const size = useWindowSize();
+
+  const handleMobileMenu = () => {
+    setMobileMenu(!isMobileMenu);
+  };
+
   return (
     <S.ContainerHeader>
       <Container>
-        <Grid container>
-          <Grid item md={8} xs={12}>
+        <Grid container className="content">
+          <Grid item md={8}>
             <S.Logo>
               <img src={Logo} alt="Logotipo com a escrita WColognesi" />
             </S.Logo>
           </Grid>
-          <Grid item md={4} xs={12}>
-            <ul>
-              <li>
+          {size.width > 900 ? (
+            <Grid item md={4}>
+              <ul>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/resumo">Resumo</a>
+                </li>
+                <li>
+                  <a href="/projetos">Projetos</a>
+                </li>
+                <li>
+                  <a href="/contato">Contato</a>
+                </li>
+              </ul>
+            </Grid>
+          ) : (
+            <Grid item md={4}>
+              <MenuIcon onClick={handleMobileMenu} />
+            </Grid>
+          )}
+          {isMobileMenu && size.width < 900 && (
+            <Grid item xs={12}>
+              <S.HeaderMobile>
                 <a href="/">Home</a>
-              </li>
-              <li>
+
                 <a href="/resumo">Resumo</a>
-              </li>
-              <li>
+
                 <a href="/projetos">Projetos</a>
-              </li>
-              <li>
+
                 <a href="/contato">Contato</a>
-              </li>
-            </ul>
-          </Grid>
+              </S.HeaderMobile>
+            </Grid>
+          )}
         </Grid>
       </Container>
     </S.ContainerHeader>
--- src/Pages/Contato/styles.js
+++ src/Pages/Contato/styles.js
@@ -2,4 +2,7 @@ import styled from "styled-components";
 
 export const ContainerContactBox = styled.section`
   padding: 46px 0 46px;
+  .MuiContainer-root {
+    margin-top: 80px;
+  }
 `;
